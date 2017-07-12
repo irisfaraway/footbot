e7eb51f5ca9a43b447f46c4619618df27433dfe9
irisfaraway <iris.faraway@gmail.com>
2017-07-12 15:08:00 +0100

    bot responds to comments

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 var Bot = require('slackbots');
 
 var settings = {
-  token: process.env.FOOTBOT_SLACK_API_TOKEN,
-  name: 'footbot'
+  token: process.env.FOOTBOT_SLACK_API_TOKEN
 };
 
 var bot = new Bot(settings);
 
+var params = {
+  as_user: true
+}
+
+// Checks on messages
+
+// Make sure the message is a message (really)
+function isMessage(message) {
+  return (message.type === 'message');
+}
+
+// Make sure the message isn't from the bot itself
+function notMyMessage(botId, message) {
+  if (isMessage(message)) {
+    return (message.user !== botId);
+  };
+};
+
+// Bot doing stuff
+
 bot.on('start', function() {
-  bot.postMessageToChannel('test', 'Footbot ACTIVATE!');
+  bot.postMessageToChannel('test', 'Did you see that ludicrous display last night?', params);
+});
+
+bot.on('message', function(message) {
+  if (notMyMessage(bot.self.id, message)) {
+    bot.postMessageToChannel('test', "The thing about Arsenal is they always try to walk it in", params);
+  }
 });
